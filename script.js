cd9ab60a8668fd3cbf96a20441f5ca012c789067
Ravenbtw <ingipoulsenfo@gmail.com>
2020-04-18 02:25:21 +0100

    Added query string that auto searches for users.
    When you search a user, it adds the query to the URL.
    
    When you visit the site with the query, it automatically searches it for you.

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ let channelName;
 let foundChannels;
 let page;
 
-function findID(name) {
+let starterName = location.search.substring(1);
+
+if (starterName) {
+  findID(starterName);
+}
+
+function findID(name, addToParam) {
+  if (addToParam) {
+    history.pushState(null, null, '?' + name);
+  }
+  input.value = name;
   fetch('https://api.twitch.tv/helix/users?login=' + name, {
     headers: {
       'Client-ID': 'cclk5hafv1i7lksfauerry4w7ythu2'
@@ -26,55 +36,55 @@ function findID(name) {
     } else {
       searchInfo.innerHTML = 'CHANNEL "' + name.toUpperCase() + '" COULD NOT BE FOUND';
     }
-  })
+  });
 }
 
 function insertChannelHTML(id, page) {
   fetch('https://api.twitch.tv/kraken/users/' + id + '/follows/channels?limit=100&offset=' + 100*page, {
-      headers: {
-        'Accept': 'application/vnd.twitchtv.v5+json',
-        'Client-ID': 'cclk5hafv1i7lksfauerry4w7ythu2'
-      }})
-      .then((response) => response.json())
-      .then((result) => {
-        if (result._total > 0) {
-          result.follows.forEach((follow, index) => {
-            foundChannels++;
-            var html = `
-              <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
-              <a href="${follow.channel.url}" class="text-white" target="_blank">
-                <div class="card primary">
-                  <img class="card-img-top" src="${follow.channel.logo}">
-                  <div class="card-body">
-                    <h4>${follow.channel.display_name}</h4>
-                    <h5>Followed on:<br>` + follow.created_at.replace('T', '<br>').replace('Z', '') + `</h5>
-                  </div>
-                </div>
-                </a>
+    headers: {
+      'Accept': 'application/vnd.twitchtv.v5+json',
+      'Client-ID': 'cclk5hafv1i7lksfauerry4w7ythu2'
+    }
+  })
+  .then((response) => response.json())
+  .then((result) => {
+    if (result._total > 0) {
+      result.follows.forEach((follow, index) => {
+        foundChannels++;
+        var html = `
+          <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+          <a href="${follow.channel.url}" class="text-white" target="_blank">
+            <div class="card primary">
+              <img class="card-img-top" src="${follow.channel.logo}">
+              <div class="card-body">
+                <h4>${follow.channel.display_name}</h4>
+                <h5>Followed on:<br>` + follow.created_at.replace('T', '<br>').replace('Z', '') + `</h5>
               </div>
-            `;
+            </div>
+            </a>
+          </div>
+        `;
 
-            followList.insertAdjacentHTML('beforeend', html);
-          });
-        } else {
-          followList.innerHTML = '';
-          searchInfo.innerHTML = input.value.replace(/ /g, '').toUpperCase() + ' IS NOT FOLLOWING ANYONE';
-        }
+        followList.insertAdjacentHTML('beforeend', html);
+      });
+    } else {
+      followList.innerHTML = '';
+      searchInfo.innerHTML = input.value.replace(/ /g, '').toUpperCase() + ' IS NOT FOLLOWING ANYONE';
+    }
 
-        if (page < Math.ceil(result._total/100)) {
-          page++;
-          searchInfo.innerHTML = input.value.toUpperCase() + ' IS FOLLOWING ' + foundChannels + ' PEOPLE:';
-          insertChannelHTML(id, page);
-        }
-      })
-
+    if (page < Math.ceil(result._total/100)) {
+      page++;
+      searchInfo.innerHTML = input.value.toUpperCase() + ' IS FOLLOWING ' + foundChannels + ' PEOPLE:';
+      insertChannelHTML(id, page);
+    }
+  })
 }
 
 input.onkeyup = (event) => {
   if (event.which == 13) {
-    var name = input.value.replace(/ /g, '');
-    if (name != '') {
-      findID(name);
+    var name = input.value.replace(/\W/g, '');;
+    if (name.length > 0) {
+      findID(name, true);
     }
   }
 }
