4fb39fe0533f6cf2d20863d6f22b9fe1a02951e3
Ravenbtw <ravenbtwbtw@gmail.com>
2021-12-13 13:53:58 +0000

    Added randomly embedding TDPA channel.

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.use(expressLayouts);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('following', { tool: 'following' }));
+const featuredChannels = ['twitchdoespixelart', 'twitchdoespixelart2', 'twitchdoespixelart3', 'twitchdoespixelart4'];
 
-app.get('/first', (req, res) => res.render('first', { tool: 'first' }));
+app.get('/', (req, res) => res.render('following', { tool: 'following', featuredChannel: featuredChannels[Math.floor(Math.random() * featuredChannels.length)] }));
+
+app.get('/first', (req, res) => res.render('first', { tool: 'first', featuredChannel: featuredChannels[Math.floor(Math.random() * featuredChannels.length)] }));
 
 app.listen(8081);
