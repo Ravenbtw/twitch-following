2e92e051ca5e2d7f5e26cc0ffc81f9dd1a26cbd6
Ravenbtw <ravenbtw@protonmail.com>
2022-07-02 21:27:48 +0100

    Redirect without showing the loading messages.

--- index.js
+++ index.js
@@ -9,30 +9,35 @@ app.use(expressLayouts);
 
 app.use(express.static('public'));
 
-const featuredChannels = ['twitchdoespixelart', 'twitchdoespixelart2', 'twitchdoespixelart3', 'twitchdoespixelart4'];
+const featuredChannels = ['streamcanvas'];
 
 app.use((req, res, next) => {
-    res.locals.showFirstFollowersButton = new Date() < new Date('02/20/2022');
-    next();
+  res.locals.showFirstFollowersButton = new Date() < new Date('02/20/2022');
+  next();
 });
 
 app.get('/', (req, res) => {
-    res.render('following', { tool: 'following', featuredChannel: featuredChannels[Math.floor(Math.random() * featuredChannels.length)] });
+  res.render('following', {
+    tool: 'following',
+    featuredChannel:
+      featuredChannels[Math.floor(Math.random() * featuredChannels.length)],
+  });
 });
 
 app.use((req, res, next) => {
-    if (new Date() > new Date('02/20/2022')) {
-        res.redirect('https://www.twitchdatabase.com');
-    } else {
-        next();
-    }
+  if (new Date() > new Date('02/20/2022')) {
+    res.redirect('https://www.twitchdatabase.com');
+  } else {
+    next();
+  }
 });
 
 app.get('/first', (req, res) => {
-    res.render('first', {
-        tool: 'first',
-        featuredChannel: featuredChannels[Math.floor(Math.random() * featuredChannels.length)]
-    });
+  res.render('first', {
+    tool: 'first',
+    featuredChannel:
+      featuredChannels[Math.floor(Math.random() * featuredChannels.length)],
+  });
 });
 
 app.listen(8083);
